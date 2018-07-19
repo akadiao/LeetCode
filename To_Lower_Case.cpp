93a19edc04f4c220974295b51d24e3edac3bf5cf
akadiao <31753531+akadiao@users.noreply.github.com>
2018-07-19 16:49:33 +0800

    Update To_Lower_Case.cpp

--- To_Lower_Case.cpp
+++ To_Lower_Case.cpp
@@ -1,3 +1,21 @@
+/*
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+
+*/
+
 #include <stdio.h>
 #include <string>
 #include <iostream>
